efbeeb98ff02f3a4abfcc0029ad9611f347a9d51
Gabriel Mendieta Hernandez <gmendieta4109@gmail.com>
2025-05-11 14:29:48 -0500

    Made the if statements more readable

--- __main__.py
+++ __main__.py
@@ -23,11 +23,10 @@ def main():
     if len(sys.argv) == 1:
         # No args were provided
         return exec_only_exec_in_cl(DIR, TOKEN)
-    elif any(i == TOKEN for i in sys.argv):
+    if any(i == TOKEN for i in sys.argv):
         return exec_token_in_cl(DIR, TOKEN, sys.argv[1:])
-    else:
-        # User provided extra arguments
-        return exec_append_entry_to_args(DIR, TOKEN, sys.argv[1:])
+    # User provided extra arguments
+    return exec_append_entry_to_args(DIR, TOKEN, sys.argv[1:])
 
 
 if __name__ == "__main__":
